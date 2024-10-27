a78a1f6a28fe593a288b1ae315b4a33ae5aaa3c0
Nitesh Muchhal <130092906+Nitesh-Muchhal2002@users.noreply.github.com>
2024-10-27 23:41:47 +0530

    Update sum and fact.cpp

--- sum and fact.cpp
+++ sum and fact.cpp
@@ -16,30 +16,30 @@ int main()
 	cin>>n;
 	printSum(n,0);
 }
-//int fact(int n)
-//{
-//	if(n==0)
-//	return 1;
-//	return n*fact(n-1);
-//}
-//int main()
-//{
-//	int n;
-//	cin>>n;
-//	cout<<fact(n);
-//}
-//int main() {
-//    // Complete the code.
-//    int a;
-//    float b;
-//    long c;
-//    double d;
-//    char ch;
-//    cin>>a>>b>>c>>d>>ch;
-//    cout<<a<<endl;
-//    cout<<b<<endl;
-//    cout<<c<<endl;
-//    cout<<d<<endl;
-//    cout<<ch;
-//    return 0;
-//}
+int fact(int n)
+{
+	if(n==0)
+	return 1;
+	return n*fact(n-1);
+}
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<fact(n);
+}
+int main() {
+   // Complete the code.
+   int a;
+   float b;
+   long c;
+   double d;
+   char ch;
+   cin>>a>>b>>c>>d>>ch;
+   cout<<a<<endl;
+   cout<<b<<endl;
+   cout<<c<<endl;
+   cout<<d<<endl;
+   cout<<ch;
+   return 0;
+}
